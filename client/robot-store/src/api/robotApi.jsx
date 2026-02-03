7880b178971b6fe0097854c0893979d9655955d0
miriam <p029919197@gmail.com>
2026-02-03 02:11:50 +0200

    fixed api url

--- client/robot-store/src/api/robotApi.jsx
+++ client/robot-store/src/api/robotApi.jsx
@@ -1,10 +1,12 @@
 import axios from "axios"
-const url = "https://robot-store-backend.onrender.com/api/robotApi";
+
+// כתובת השרת ב-Render
+const url = "https://robot-store-backend.onrender.com";
 
 //חיפוש כל הרובוטים
 export const getAll = async() => {
     try {
-        const {data}=await axios.get(`${url}/getAllRobots`)
+        const {data} = await axios.get(`${url}/getAllRobots`)
         return data;
     }
     catch(e) {
@@ -14,24 +16,25 @@ export const getAll = async() => {
 
 //חיפוש רובוט לפי קוד
 export const getById = async(id) => {
-        try {
-        const {data}=await axios.get(`${url}/getRobotById/${id}`)
+    try {
+        const {data} = await axios.get(`${url}/getRobotById/${id}`)
         return data;
     }
     catch(e) {
         console.log(e)
     }
-
 }
 
 //הוספת רובוט
 export const addRobot = async(robot) => {
     try {
-        const {data}=await axios.post(`${url}/postRobot`,robot)
+        const {data} = await axios.post(`${url}/postRobot`, robot)
         return data;
     }
     catch(e) {
         console.log(e)
     }
 }
-export const getImageUrl = (img) => `https://robot-store-backend.onrender.com/${img}`;
+
+// פונקציה לקבלת נתיב התמונה מהשרת
+export const getImageUrl = (img) => `${url}/${img}`;
